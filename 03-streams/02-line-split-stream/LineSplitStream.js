ad01191a1b8c39040786c069d2c578efcdc3a3f0
Alexandr Kazakov <alexandr.kazakov1@gmail.com>
2020-08-09 22:51:51 +0300

    Add Solution for 03/02

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -7,10 +7,17 @@ class LineSplitStream extends stream.Transform {
   }
 
   _transform(chunk, encoding, callback) {
+    const string = chunk.toString();
+    const array = string.split(`${os.EOL}`);
+
+    for (let i = 0; i < array.length; i++) {
+      this.push(array[i]);
+    }
+
+    callback();
   }
 
-  _flush(callback) {
-  }
+  _flush(callback) {}
 }
 
 module.exports = LineSplitStream;
