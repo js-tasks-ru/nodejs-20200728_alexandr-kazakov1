b824ba9ca299b898d20e73b56bae11f8bec58e39
Alexandr Kazakov <alexandr.kazakov1@gmail.com>
2021-02-15 12:50:53 +0300

    Complete 05-03

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -2,16 +2,23 @@ const url = require('url');
 const http = require('http');
 const path = require('path');
 
+const deleteFile = require('./deleteFile');
+
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+
+  if (pathname.includes('/') || pathname.includes('..')) {
+    res.statusCode = 400;
+    res.end('Nested paths are not allowed');
+    return;
+  }
 
   switch (req.method) {
     case 'DELETE':
-
+      deleteFile(filepath, req, res);
       break;
 
     default:
